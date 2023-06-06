9f3c5a0b44ed440b49454aecac4d630c3017f145
Meera Aljneibi <mealjnei@lab2r4s3.42abudhabi.ae>
2023-06-06 14:11:21 +0400

    42 C++

--- Module 03/ex02/main.cpp
+++ Module 03/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mealjnei <mealjnei@student.42.ae>          +#+  +:+       +#+        */
+/*   By: mealjnei <mealjnei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 16:48:29 by mealjnei          #+#    #+#             */
-/*   Updated: 2023/06/05 17:00:44 by mealjnei         ###   ########.fr       */
+/*   Updated: 2023/06/06 13:07:02 by mealjnei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,43 @@
 
 int main()
 {
-    // ClapTrap a("Jhon");
-    // ClapTrap b("Enemy");
-    // std::cout << std::endl;
-    // a.attack("Enemy");
-    // b.attack("Jhon");
-    // std::cout << std::endl;
-    // a.takeDamage(10);
-    // b.takeDamage(10);
-    // std::cout << std::endl;
-    // a.beRepaired(10);
-    // b.beRepaired(10);
-    // std::cout << std::endl;
+    ClapTrap a("Jhon");
+    ClapTrap b("Enemy");
+    std::cout << std::endl;
+    a.attack("Enemy");
+    b.attack("Jhon");
+    std::cout << std::endl;
+    a.takeDamage(10);
+    b.takeDamage(10);
+    std::cout << std::endl;
+    a.beRepaired(10);
+    b.beRepaired(10);
+    std::cout << std::endl;
 
-    // ScavTrap c("Alex");
-    // ScavTrap d("Enemy2");
-    // std::cout << std::endl;
-    // c.attack("Enemy2");
-    // d.attack("Alex");
-    // std::cout << std::endl;
-    // c.takeDamage(10);
-    // d.takeDamage(10);
-    // std::cout << std::endl;
-    // c.beRepaired(10);
-    // d.beRepaired(10);
-    // std::cout << std::endl;
+    ScavTrap c("Alex");
+    ScavTrap d("Enemy2");
+    std::cout << std::endl;
+    c.attack("Enemy2");
+    d.attack("Alex");
+    std::cout << std::endl;
+    c.takeDamage(10);
+    d.takeDamage(10);
+    std::cout << std::endl;
+    c.beRepaired(10);
+    d.beRepaired(10);
+    std::cout << std::endl;
 
     FragTrap e("Sam");
-    FragTrap f=e;
-    // FragTrap f("Enemy3");
-    // std::cout << std::endl;
-    // e.attack("Enemy3");
-    // f.attack("Sam");
-    // std::cout << std::endl;
-    // e.takeDamage(10);
-    // f.takeDamage(10);
-    // std::cout << std::endl;
-    // e.beRepaired(10);
-    // f.beRepaired(10);
-    // std::cout << std::endl;
+    FragTrap f("Enemy3");
+    std::cout << std::endl;
+    e.attack("Enemy3");
+    f.attack("Sam");
+    std::cout << std::endl;
+    e.takeDamage(10);
+    f.takeDamage(10);
+    std::cout << std::endl;
+    e.beRepaired(10);
+    f.beRepaired(10);
+    std::cout << std::endl;
     return (0);
 }
