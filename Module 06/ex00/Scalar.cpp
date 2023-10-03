15df7aed5a5ca0b133b81b1e8b3593628302a0ed
Meera <meraaljneibi@gmail.com>
2023-10-03 14:05:42 +0400

    42

--- Module 06/ex00/Scalar.cpp
+++ Module 06/ex00/Scalar.cpp
@@ -1,5 +1,3 @@
-
-// Converter.cpp
 #include "Scalar.hpp"
 
 Scalar::Scalar() {}
@@ -13,68 +11,64 @@ Scalar &Scalar::operator=(Scalar const &data)
 {
     if (this != &data)
         this->m_literal = data.m_literal;
+
     return *this;
 }
 
 Scalar::~Scalar() {}
 
- //function to set the value of m_literal with the input string
-void Scalar::setLiteral(std::string literal)
-{
+void Scalar::setLiteral(std::string literal) {
     this->m_literal = literal;
 }
 
-//converting the stored m_literal into different data types (convertion process)
-void Scalar::convert()
-{
+void Scalar::convert() {
     std::cout << "Scalar::convert" << std::endl;
     std::cout << m_literal << std::endl;
-    if (m_literal == "0")
+    if (m_literal == "0") {
         printForInt(0);
-    else if(isChar())
+    } else if(isChar()) {
         printForChar(m_literal[0]);
-    else if(isInt())
+    } else if(isInt()) {
         printForInt(std::atoi(m_literal.c_str()));
-    else if(isFloat())
+    } else if(isFloat()) {
         printForFloat(std::atof(m_literal.c_str()));
-    else if(isDouble())
+    } else if(isDouble()) {
         printForDouble(std::atof(m_literal.c_str()));
-    else
+    } else {
         std::cerr << "Invalid Literal" << std::endl;
+    }
 }
 
-void Scalar::processConversion()
-{
-    if (this->isInt())
+void Scalar::processConversion() {
+    if (this->isInt()) {
         this->printForInt(std::stoi(this->m_literal));
-    else if (this->isFloat())
+    } else if (this->isFloat()) {
         this->printForFloat(std::stof(this->m_literal));
-    else if (this->isDouble())
+    } else if (this->isDouble()) {
         this->printForDouble(std::stod(this->m_literal));
-    else if (this->isChar())
+    } else if (this->isChar()) {
         this->printForChar(this->m_literal[0]);
-    else
+    } else {
         std::cerr << "Error: The provided literal does not match any expected formats." << std::endl;
+    }
 }
 
 
-bool Scalar::isChar() const
-{
+bool Scalar::isChar() const {
     return m_literal.size() == 1 && std::isprint(m_literal[0]) && !std::isdigit(m_literal[0]);
 }
 
-bool Scalar::isInt() const
-{
+bool Scalar::isInt() const {
     char *endptr;
     long val = std::strtol(m_literal.c_str(), &endptr, 10);
-    if (val == 0 && m_literal == "0")
+    if (val == 0 && m_literal == "0") {
         return true;
+    }
     return *endptr == '\0';
 }
 
 
-bool Scalar::isFloat() const
-{
+bool Scalar::isFloat() const {
     size_t len = m_literal.length();
     if (len == 0) return false;
 
@@ -93,8 +87,7 @@ bool Scalar::isFloat() const
     return *endptr == 'f' || *endptr == 'F';
 }
 
-bool Scalar::isDouble() const
-{
+bool Scalar::isDouble() const {
     // Check for special double literals
     if (m_literal == "-inf" || m_literal == "+inf" || m_literal == "nan")
         return true;
@@ -105,62 +98,64 @@ bool Scalar::isDouble() const
     return *endptr == '\0';
 }
 
-void Scalar::printForChar(char c) const
-{
+void Scalar::printForChar(char c) const {
     std::cout << "char: ";
-    if (std::isprint(c))
+    if (std::isprint(c)) {
         std::cout << "'" << c << "'" << std::endl;
-    else
+    } else {
         std::cout << "Non displayable" << std::endl;
+    }
     std::cout << "int: " << static_cast<int>(c) << std::endl;
     std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
     std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
 }
 
-void Scalar::printForInt(int i) const
-{
+void Scalar::printForInt(int i) const {
     std::cout << "char: ";
-    if (i == 0)
+    if (i == 0) {
         std::cout << "Non displayable" << std::endl;
-    else if (i >= 32 && i <= 126) // Printable ASCII range
+    } else if (i >= 32 && i <= 126) {  // Printable ASCII range
         std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
-    else
+    } else {
         std::cout << "Non displayable" << std::endl;
+    }
     std::cout << "int: " << i << std::endl;
     std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
     std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
 }
 
-void Scalar::printForFloat(float f) const
-{
+void Scalar::printForFloat(float f) const {
     std::cout << "char: ";
     int i = static_cast<int>(f);
-    if (i >= 32 && i <= 126)  // Printable ASCII range
+    if (i >= 32 && i <= 126) {  // Printable ASCII range
         std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
-    else
+    } else {
         std::cout << "impossible" << std::endl;
+    }
     std::cout << "int: ";
-    if (f < INT_MIN || f > INT_MAX)
+    if (f < INT_MIN || f > INT_MAX) {
         std::cout << "impossible" << std::endl;
-    else
+    } else {
         std::cout << i << std::endl;
+    }
     std::cout << "float: " << f << "f" << std::endl;
     std::cout << "double: " << static_cast<double>(f) << std::endl;
 }
 
-void Scalar::printForDouble(double d) const
-{
+void Scalar::printForDouble(double d) const {
     std::cout << "char: ";
     int i = static_cast<int>(d);
-    if (i >= 32 && i <= 126) // Printable ASCII range
+    if (i >= 32 && i <= 126) {  // Printable ASCII range
         std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
-    else
+    } else {
         std::cout << "impossible" << std::endl;
+    }
     std::cout << "int: ";
-    if (d < INT_MIN || d > INT_MAX)
+    if (d < INT_MIN || d > INT_MAX) {
         std::cout << "impossible" << std::endl;
-    else
+    } else {
         std::cout << i << std::endl;
+    }
     std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
     std::cout << "double: " << d << std::endl;
 }
